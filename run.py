87c4f5b9bdde13ba9d5f61ca57abfaaa6bd0b6a2
arturylab <arturylab@gmail.com>
2025-06-25 17:01:19 -0700

    Deploy Oracle Cloud test

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     with app.app_context():
         from app.models import db
         db.create_all()
-    app.run(debug=True, port=5005)
+    app.run(debug=False, host='0.0.0.0', port=5005)
